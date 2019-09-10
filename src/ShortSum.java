5706d844f783154f8659e346e2b5fd75b82b2cf3
William Mueller <william.mueller@bishopireton.org>
2019-09-10 12:01:08 -0400

    added other math stuff

--- src/ShortSum.java
+++ src/ShortSum.java
@@ -14,12 +14,15 @@ public class ShortSum {
 		Scanner kb = new Scanner(System.in);
 		
 		System.out.print("Please enter a number: ");
-		int num1 = kb.nextInt();
+		double num1 = kb.nextInt();
 		
 		System.out.print("Please enter another number: ");
-		int num2 = kb.nextInt();
+		double num2 = kb.nextInt();
 		
 		System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1+num2));
+		System.out.println("The difference of " + num1 + " and " + num2 + " is: " + (num1-num2));
+		System.out.println("The product of " + num1 + " and " + num2 + " is: " + (num1*num2));
+		System.out.println("The quotient of " + num1 + " and " + num2 + " is: " + (num1/num2));
 		
 	}
 
