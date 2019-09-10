2b0e8bd33634a0f81039203bf1a0e3606509b009
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2019-09-10 11:58:45 -0400

    Made sum a method and added digsig

--- src/ShortSum.java
+++ src/ShortSum.java
@@ -1,5 +1,6 @@
 /**
  * @author William Mueller
+ *gangity gang i would come up with a rhyme but gangity gang i do not seem to have the time
  * #qwerty:P
  */
 
@@ -19,9 +20,14 @@ public class ShortSum {
 		System.out.print("Please enter another number: ");
 		int num2 = kb.nextInt();
 		
-		System.out.println("The sum is: " + (num1+num2));
+		System.out.println("The sum is: " + sum(num1, num2));
 		
 	}
 
+	public static int sum(int x, int y)
+	{
+		return x + y;
+	}
+	
 }
 
